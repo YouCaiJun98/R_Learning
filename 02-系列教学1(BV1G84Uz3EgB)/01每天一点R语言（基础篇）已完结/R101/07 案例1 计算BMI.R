2589a5ac50008081b7d3ee7f30cd3b97708645c1
YouCaiJun98 <704080844@qq.com>
2026-03-24 13:00:33 +0800

    add 3 lessons

---
+++ 02-系列教学1(BV1G84Uz3EgB)/01每天一点R语言（基础篇）已完结/R101/07 案例1 计算BMI.R
@@ -0,0 +1,18 @@
+# 读取数据
+data_BC_BMI = read.csv('./data_case_day7.csv', row.names=1)
+data_BC_BMI["P004", ]
+
+# 提取数据
+data_BC_BMI$W
+data_BC_BMI$H
+
+# 做向量运算
+BMI = data_BC_BMI$W / data_BC_BMI$H ^ 2
+
+# 将BMI存到数据框中
+data_BC_BMI = data.frame(data_BC_BMI, BMI)
+## 方法二
+# data_BC_BMI$BMI = BMI
+
+# 数据导出
+write.csv(data_BC_BMI, "./data_case_day7_BMI.csv")
