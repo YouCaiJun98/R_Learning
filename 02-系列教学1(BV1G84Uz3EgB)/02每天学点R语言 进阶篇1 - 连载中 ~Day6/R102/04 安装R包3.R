eb0b73a244128d5cb286cbab2060d40646516299
YouCaiJun98 <704080844@qq.com>
2026-03-30 01:02:49 +0800

    finish R installation lessons

--- 02-系列教学1(BV1G84Uz3EgB)/02每天学点R语言 进阶篇1 - 连载中 ~Day6/R102/04 安装R包3.R
+++ 02-系列教学1(BV1G84Uz3EgB)/02每天学点R语言 进阶篇1 - 连载中 ~Day6/R102/04 安装R包3.R
@@ -1,7 +1,17 @@
 install.packages("devtools")
 library("devtools");
-install.packages("Rtools")
+# 这个需要下载exe安装
+# install.packages("Rtools")
 install.packages("bluster")
+
+# 设置本地代理（需要先挂梯子）
+## 设置HTTP代理
+Sys.setenv(http_proxy = "http://127.0.0.1:7897")
+## 设置HTTPS代理
+Sys.setenv(https_proxy = "http://127.0.0.1:7897")
+library(usethis)
+
+
 install_github("Danko-Lab/BayesPrism/BayesPrism")
 
 library(BayesPrism)
